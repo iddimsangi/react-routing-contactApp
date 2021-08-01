8636216ccbff8f56e22fb4e7eaba81af0da657b1
iddimsangi <adammsangi@gmail.com>
2021-08-01 13:26:16 +0300

    add a modal when delete button is clicked

---
+++ src/components/AlertDelete/AlertDelete.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AlertDelete() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AlertDelete
--- src/components/ContactsList/ContactCard/ContactCard.js
+++ src/components/ContactsList/ContactCard/ContactCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
 function ContactCard(props) {
+    const [open, setOpen] = React.useState(false)
     const{id, name, email } = props.contact;
     // console.log(props.contact)
-    console.log(props.contact.name);
-    console.log(props.contact.email);
+    // console.log(props.contact.name);
+    // console.log(props.contact.email);
     const style = {
         width:"100%",
         backgroundColor:'rgb(38, 88, 88)',
@@ -26,10 +27,46 @@ function ContactCard(props) {
             <div role="list" class="ui divided middle aligned list" style={style}>
                 <div role="listitem" class="item">
                     <div class="right floated content">
-                        <button 
+                    <Modal
+                            onClose={() => setOpen(false)}
+                            onOpen={() => setOpen(true)}
+                            open={open}
+                            trigger={<Button color='red'>Delete</Button>}
+                            >
+                            <Modal.Header>Delete Contact</Modal.Header>
+                            <Modal.Content image>
+                                {/* <Image size='medium' src='/images/avatar/large/rachel.png' wrapped /> */}
+                                <Modal.Description>
+                                {/* <Header</Header> */}
+                                <p>
+                                    Are you sure you want to delete {name} contact?
+                                </p>
+                                {/* <p>Is it okay to use this photo?</p> */}
+                                </Modal.Description>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color='black' onClick={() => setOpen(false)}>
+                                Nope
+                                </Button>
+                                <Button
+                                content="Yep I'm sure!"
+                                labelPosition='right'
+                                icon='checkmark'
+                                onClick = {() =>props.removeContact(id)}
+                                // onClick={() => setOpen(false) && props.removeContact(id)}
+                                positive
+                                />
+                            </Modal.Actions>
+                    </Modal>
+
+
+
+
+
+                        {/* <button 
                         class="ui red button"
                         onClick = {() =>props.removeContact(id)}
-                        >delete</button>
+                        >delete</button> */}
                         </div><img src="https://react.semantic-ui.com/images/avatar/small/lena.png" class="ui avatar image"/>
                         <div class="content">{name}</div>
                         <a href="#" style={emailStyle} >{email}</a>
